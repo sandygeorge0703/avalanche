a2a95a3e26acba44e71688f5f7517186995c4b8d
sandygeorge0703 <sandhrageorge96@gmail.com>
2024-10-07 16:38:28 +0100

    Doctest issue fixed.

--- Basic_CNN.py
+++ Basic_CNN.py
@@ -9,13 +9,13 @@
 # Website: avalanche.continualai.org                                           #
 ################################################################################
 
+
 import torch.nn as nn
 
 from avalanche.models.dynamic_modules import (
     MultiTaskModule,
     MultiHeadClassifier,
 )
-
 
 class SimpleCNN(nn.Module):
     """
@@ -26,35 +26,61 @@ class SimpleCNN(nn.Module):
         >>> from avalanche.models import SimpleCNN
         >>> n_classes = 10 # e.g. MNIST
         >>> model = SimpleCNN(num_classes=n_classes)
-        >>> print(model) # View model details
+        >>> print(model)  # View model details
+        SimpleCNN(
+          (features): Sequential(
+            (0): Conv2d(3, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            (1): ReLU(inplace=True)
+            (2): Conv2d(32, 32, kernel_size=(3, 3), stride=(1, 1))
+            (3): ReLU(inplace=True)
+            (4): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            (5): Dropout(p=0.25, inplace=False)
+            (6): Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            (7): ReLU(inplace=True)
+            (8): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1))
+            (9): ReLU(inplace=True)
+            (10): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+            (11): Dropout(p=0.25, inplace=False)
+            (12): Conv2d(64, 64, kernel_size=(1, 1), stride=(1, 1))
+            (13): ReLU(inplace=True)
+            (14): AdaptiveMaxPool2d(output_size=1)
+            (15): Dropout(p=0.25, inplace=False)
+          )
+          (classifier): Sequential(
+            (0): Linear(in_features=64, out_features=10, bias=True)
+          )
+        )
     """
+
 
     def __init__(self, num_classes=10):
         super(SimpleCNN, self).__init__()
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),  # Conv Layer 1
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, padding=0),
+            nn.Conv2d(32, 32, kernel_size=3, padding=0),           # Conv Layer 2
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(p=0.25),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),                 # Max Pooling 1
+            nn.Dropout(p=0.25),                                    # Dropout
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),           # Conv Layer 3
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=0),
+            nn.Conv2d(64, 64, kernel_size=3, padding=0),           # Conv Layer 4
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(p=0.25),
-            nn.Conv2d(64, 64, kernel_size=1, padding=0),
+            nn.MaxPool2d(kernel_size=2, stride=2),                 # Max Pooling 2
+            nn.Dropout(p=0.25),                                    # Dropout
+            nn.Conv2d(64, 64, kernel_size=1, padding=0),           # Conv Layer 5
             nn.ReLU(inplace=True),
-            nn.AdaptiveMaxPool2d(1),
-            nn.Dropout(p=0.25),
+            nn.AdaptiveMaxPool2d(1),                               # Adaptive Pooling
+            nn.Dropout(p=0.25),                                    # Dropout
         )
-        self.classifier = nn.Sequential(nn.Linear(64, num_classes))
+        self.classifier = nn.Sequential(
+            nn.Linear(64, num_classes)                             # Classifier
+        )
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
+        x = x.view(x.size(0), -1)  # Flatten
         x = self.classifier(x)
         return x
 
@@ -66,14 +92,16 @@ class MTSimpleCNN(SimpleCNN, MultiTaskModule):
     """
 
     def __init__(self):
-        super().__init__()
-        self.classifier = MultiHeadClassifier(64)
+        super(MTSimpleCNN, self).__init__()
+        self.classifier = MultiHeadClassifier(64)  # Multi-head classifier for different tasks
 
     def forward(self, x, task_labels):
         x = self.features(x)
-        x = x.squeeze()
-        x = self.classifier(x, task_labels)
+        x = x.squeeze()  # Remove dimensions of size 1 (for multi-task setup)
+        x = self.classifier(x, task_labels)  # Pass task labels for multi-task learning
         return x
 
 
+# Ensuring __all__ includes both models for easy import
 __all__ = ["SimpleCNN", "MTSimpleCNN"]
+
