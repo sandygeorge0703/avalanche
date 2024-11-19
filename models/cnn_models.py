a22652e79c29da348878c392b47a28344cd84ed8
sandygeorge0703 <sandhrageorge96@gmail.com>
2024-11-19 08:56:05 +0000

    Drop out layers removed. Print of three-class outputs from model during training and validation added. Confusion matrix added.

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -15,17 +15,14 @@ class SimpleCNN(nn.Module):
             nn.Conv2d(32, 32, kernel_size=3, padding=0),           # Conv Layer 2
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),                 # Max Pooling 1
-            nn.Dropout(p=0.25),                                    # Dropout
             nn.Conv2d(32, 64, kernel_size=3, padding=1),           # Conv Layer 3
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=0),           # Conv Layer 4
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),                 # Max Pooling 2
-            nn.Dropout(p=0.25),                                    # Dropout
             nn.Conv2d(64, 64, kernel_size=1, padding=0),           # Conv Layer 5
             nn.ReLU(inplace=True),
             nn.AdaptiveMaxPool2d(1),                               # Adaptive Pooling
-            nn.Dropout(p=0.25),                                    # Dropout
         )
         self.classifier = nn.Sequential(
             nn.Linear(64, num_classes)                             # Classifier
@@ -36,3 +33,4 @@ class SimpleCNN(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten
         x = self.classifier(x)
         return x
+
